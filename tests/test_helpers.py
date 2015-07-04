9e834411442d0d2e762f3d2ac10dc91d8331d055
Jan Karásek <devel@hkar.eu>
2015-07-04 16:23:39 +0200

    add loading config

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -2,7 +2,33 @@ __author__ = 'hkar'
 
 import random
 import string
+import os
+import yaml
+import pprint
 
+###
+# overwrite local config variables for travis-ci
+###
+
+config_file = 'config.yaml'
+
+if os.path.exists('test.'+config_file):
+    config_file = 'test.'+config_file
+
+with open(config_file, 'r') as stream:
+    config = yaml.load(stream)
+
+    if 'storage__s3__access_id' in os.environ:  # TODO: this is ugly...
+        config['storage']['s3']['access_id'] = os.environ['storage__s3__access_id']
+
+    if 'storage__s3__access_secret' in os.environ:
+        config['storage']['s3']['access_secret'] = os.environ['storage__s3__access_secret']
+
+pprint.pprint(config)
+
+##
+# helpers
+##
 
 def create_test_file(tmpdir):
     p = tmpdir.join("test_text.txt")
