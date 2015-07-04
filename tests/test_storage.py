9386567ef0f1920ab914a34f8ebcd3e00aa9dc8e
Jan Karásek <devel@hkar.eu>
2015-07-04 19:40:11 +0200

    remove print

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -8,8 +8,6 @@ def test_s3(tmpdir):
     s3_config = config['storage']['s3']
     storage = Vault.Storage.S3Storage(s3_config['access_id'], s3_config['access_secret'], s3_config['bucket'], s3_config['endpoint'])
 
-    print(s3_config)
-
     f = create_test_file(tmpdir)
 
     storage.save("test.txt", str(f))
