4afb92579452594f07158d647fd299d071df92a1
Jan Karásek <devel@hkar.eu>
2015-07-05 17:28:13 +0200

    add archive ZIP method

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -1,1 +1,27 @@
 __author__ = 'hkar'
+
+from test_helpers import *
+from Vault.Archive import ZipArchive
+import os
+import subprocess
+import time
+
+
+def test_zip(tmpdir):
+    f = create_test_file(tmpdir)
+
+    if not os.path.exists(str(f)):
+        assert False
+
+    tmpdir = str(tmpdir)
+
+    archive_path = tmpdir + '/archive' + ZipArchive.get_suffix()
+    test_file = tmpdir + '/test_file_backup.txt'
+
+    ZipArchive.pack(tmpdir, archive_path)
+
+    os.rename(str(f), test_file)
+
+    ZipArchive.unpack(archive_path, tmpdir)
+
+    assert True if os.path.exists(str(f)) else False
